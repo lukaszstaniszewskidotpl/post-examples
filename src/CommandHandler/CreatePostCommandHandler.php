3d8fb21ae9914badd8d0fcbfe7397b79d66ba76a
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2022-05-30 20:57:55 +0200

    bad solution

--- src/CommandHandler/CreatePostCommandHandler.php
+++ src/CommandHandler/CreatePostCommandHandler.php
@@ -5,19 +5,23 @@ namespace App\CommandHandler;
 use App\Command\CreatePostCommand;
 use App\Command\SendNewsletterCommand;
 use App\Entity\Post;
+use App\Repository\NewsletterUserRepository;
 use App\Repository\PostRepository;
 use App\Util\ChainHandlerAbstract;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Notifier\Recipient\Recipient;
 
 #[AsMessageHandler]
-final class CreatePostCommandHandler extends ChainHandlerAbstract
+final class CreatePostCommandHandler
 {
     public function __construct(
-        MessageBusInterface $commandBus,
-        private readonly PostRepository $postRepository
+        private readonly PostRepository $postRepository,
+        private readonly NewsletterUserRepository $newsletterUserRepository,
+        private readonly NotifierInterface $notifier,
     ) {
-        parent::__construct($commandBus);
     }
 
 
@@ -27,6 +31,22 @@ final class CreatePostCommandHandler extends ChainHandlerAbstract
 
         $this->postRepository->add($post);
 
-        $this->next(new SendNewsletterCommand($command->getUuid()));
+        $this->sendNewsletter($post);
+    }
+
+    private function sendNewsletter(Post $post): void
+    {
+        foreach ($this->newsletterUserRepository->findAll() as $user) {
+            $notification = (
+            new Notification(
+                'New Post on blog lukaszstaniszewski.pl!',
+                ['email'],
+            )
+            )->content($post->getTitle());
+
+            $recipient = new Recipient($user->getEmail());
+
+            $this->notifier->send($notification, $recipient);
+        }
     }
 }

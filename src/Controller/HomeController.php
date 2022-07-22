da437e0c7d2a4e60e49c27a37936fb19c45d9462
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2022-07-22 20:07:55 +0200

    add examples for frame, visit etc.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,25 @@ class HomeController extends AbstractController
     #[Route('/')]
     public function index(): Response
     {
-        return $this->render('home/home.html.twig');
+        return $this->render(
+            'home/home.html.twig',
+            [
+                'dateTimes' => [
+                    'now',
+                    '+1 hour',
+                    '+1 day',
+                    '+1 week',
+                    '+1 year',
+                ]
+            ]
+        );
+    }
+
+    #[Route('/static')]
+    public function static(): Response
+    {
+        return $this->render(
+            'home/static.html.twig', []
+        );
     }
 }
